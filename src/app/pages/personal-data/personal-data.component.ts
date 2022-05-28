5fcb2560eb2da5da9b100e0ead618c7a24b1239a
Cristobal <cristobalcamano@gmail.com>
2022-05-28 13:10:56 -0500

    vista personal data

--- src/app/pages/personal-data/personal-data.component.ts
+++ src/app/pages/personal-data/personal-data.component.ts
@@ -19,15 +19,15 @@ export class PersonalDataComponent implements OnInit {
 
   createformGroup(){
     return new FormGroup({
-      nombre: new FormControl('', [Validators.required, Validators.minLength(3), Validators.pattern(/^[a-zA-Z ]+$/)]),
-      rut: new FormControl('', [Validators.required, Validators.minLength(9), Validators.maxLength(9), Validators.pattern(/^[1-9 ]+$/)]),
-      rut2: new FormControl('', [Validators.required, Validators.minLength(1), Validators.maxLength(1), Validators.pattern(/^[1-9 ]+$/)]),
-      correo: new FormControl('', [Validators.required, Validators.email]),
-      telefono: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(3), Validators.pattern(/^[1-9 ]+$/)]),
-      telefono2: new FormControl('', [Validators.required, Validators.minLength(8), Validators.pattern(/^[1-9 ]+$/)])
+      nombre: new FormControl('', [Validators.required, Validators.minLength(6), Validators.pattern(/^[a-zA-Z]{1,12}\s([a-zA-Z\s]{1,50})$/i)]),
+      rut: new FormControl('', [Validators.required, Validators.minLength(11), Validators.maxLength(11), Validators.pattern(/^([1-9]{9}-\d)+$/)]),
+      correo: new FormControl('', [Validators.required, Validators.email, Validators.pattern(/^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i)]),
+      telefono: new FormControl('', [Validators.required, Validators.minLength(12), Validators.maxLength(18), Validators.pattern(/^(\+\s[1-9]{3}\s[1-9]{8,12})+$/)]),
     });
   }
 
+  rutValidate = '';
+  telefonoValidate = '';
   get nombre() { return this.searchForm.get('nombre'); }
   get rut() { return this.searchForm.get('rut'); }
   get correo() { return this.searchForm.get('correo'); }
@@ -41,6 +41,7 @@ export class PersonalDataComponent implements OnInit {
     this.router.routeReuseStrategy.shouldReuseRoute = function () {
       return false;
     }
+    this.rutValidate = '';
   }
 
   ngOnInit(): void {
@@ -55,4 +56,34 @@ export class PersonalDataComponent implements OnInit {
     this.viewShow.emit('amountPayable');
   }
 
+  validateRut(){
+    
+    //this.rut?.value
+        if(this.rutValidate.length == 8 && this.searchForm.get('rut')?.value.length == 9){
+          this.searchForm.controls['rut'].setValue(this.searchForm.get('rut')?.value+'-');
+        } else if(this.rutValidate.length == 10 && this.searchForm.get('rut')?.value.length == 9){
+          this.searchForm.controls['rut'].setValue(this.searchForm.get('rut')?.value.substring(0,8));
+        }
+        this.rutValidate = this.searchForm.get('rut')?.value;
+      }
+
+    validateTelefono(){
+    
+      //this.rut?.value
+      if(this.telefonoValidate.length == 0 && this.searchForm.get('telefono')?.value.length == 1){
+        this.searchForm.controls['telefono'].setValue('+ ' + this.searchForm.get('telefono')?.value);
+      } else if(this.telefonoValidate.length == 3 && this.searchForm.get('telefono')?.value.length == 2){
+      this.searchForm.controls['telefono'].setValue('');
+      }
+
+      if(this.telefonoValidate.length == 4 && this.searchForm.get('telefono')?.value.length == 5){
+        this.searchForm.controls['telefono'].setValue(this.searchForm.get('telefono')?.value + ' ');
+      } else if(this.telefonoValidate.length == 6 && this.searchForm.get('telefono')?.value.length == 5){
+      this.searchForm.controls['telefono'].setValue(this.searchForm.get('telefono')?.value.substring(0,4)); 
+      }
+
+    this.telefonoValidate = this.searchForm.get('telefono')?.value;
+  }
+  
 }
+
