10e5be6999bc2d23dfcc6e6969cac32731566c49
Cristobal <cristobalcamano@gmail.com>
2022-08-17 15:59:48 -0500

    ajustes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,5 +3,5 @@ export const environment = {
   production: false,
 
   base_url: 'http://localhost:8082/api/v1/mandatodigital/'
-  //base_url: 'http://localhost:8080/patpass/v1/api/'
+  //base_url: 'http://localhost:8080/mandatodigital/'
 };
